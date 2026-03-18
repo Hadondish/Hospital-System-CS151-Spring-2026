1fbad333b004249f36362cd65cce2d8e41aabd3e
Kevin Tran <niveknart26@gmail.com>
2026-03-17 17:09:18 -0700

    Added medication.java and surgeryBill.java

---
+++ medication.java
@@ -0,0 +1,42 @@
+// Kevin Tran
+// Medication.java
+
+/* Medication class is used to get information about medications such as medication names, medication costs, and whether insurance is provided. */
+public class medication implements billable {
+    // Declare variables
+    private String medicationName;
+
+    private int unitPrice;
+
+    private int quantity;
+
+    //Constructor for a new Medication
+    public medication(String medicationName, int unitPrice, int quantity) {
+        this.medicationName = medicationName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+
+    // Get the total cost
+    public int getCost() {
+        return unitPrice * quantity;
+    }
+
+    // Get the Description
+    public String getDescription() {
+        return "Medication " + medicationName + " | Quantity: "  + quantity;
+    }
+    // From billable.java contract
+    public String getCode() {
+        return medicationName.toUpperCase();
+    }
+
+    public String getBaseCost() {
+        return "Medication: " + medicationName + "| Total: $" + getCost();
+    }
+
+    public boolean isCoveredByInsurance() {
+        return true;
+    }
+
+}
