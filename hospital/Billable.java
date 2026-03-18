991c6658d117c97c3c95068486a7d5c90b6340b4
Kevin Tran <niveknart26@gmail.com>
2026-03-17 21:32:48 -0700

    Add Billable.java to hospital package

---
+++ hospital/Billable.java
@@ -0,0 +1,15 @@
+// Kevin Tran
+// Billable Interface.
+package hospital;
+/* Contract for items billed to hospital patients */
+
+public interface Billable {
+    // Total cost 
+    int getCost();
+
+    // base amount before total cost
+    String getBaseCost();
+
+    // True if covered by insurance, False if not covered by insurance, 
+    boolean isCoveredByInsurance();
+}
